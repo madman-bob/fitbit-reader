cfae5e4b0df2e07e7d902ae18a80c5577f0be815
madman-bob <madman.bob@hotmail.co.uk>
2020-01-21 11:59:48 +0000

    Add app listener for new feed items

--- app/feeds.js
+++ app/feeds.js
@@ -1,10 +1,23 @@
+import { me } from "appbit";
+
 import { fileCache } from "../common/cache";
 import Watcher from "../common/watcher";
 import FeedQueue from "../common/feed-queue";
+
+import messages from "./messages";
 
 let feedsRaw = fileCache("feeds.json");
 feedsRaw.items = new FeedQueue(feedsRaw.items, 10);
 
 let feeds = new Watcher(feedsRaw);
+
+messages.on("feed-item", item => {
+    feedsRaw.items.push(item);
+    feeds.emit("items");
+});
+
+me.addEventListener("unload", () => {
+    feedsRaw.save();
+});
 
 export default feeds;
